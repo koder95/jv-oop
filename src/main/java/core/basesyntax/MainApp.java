b4658ca47ce3cef7b55bff049e32181885aa1251
Kamil Jan Mularski <kammul@outlook.com>
2025-09-01 18:39:08 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,6 +6,12 @@ public class MainApp {
   }
   
   public static void main(String[] args) {
-    System.out.println("Java is the best");
+    Machine[] machines = new Machine[] {
+      new Truck(), new Bulldozer(), new Excavator()
+    };
+    for (Machine machine : machines) {
+      machine.doWork();
+      machine.stopWork();
+    }
   }
 }
